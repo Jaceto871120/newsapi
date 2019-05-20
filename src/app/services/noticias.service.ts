360f0a9b6e07220cb262992de14415b79f142ba7
Jair Armando Celis Torrado <jairarmandocelistorrado@MacBook-Pro-de-Jair.local>
2019-05-20 00:32:56 -0500

    Se modifica el pais a colombia  se cambia el nombre de la aplicacion a NEWS

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -33,19 +33,19 @@ export class NoticiasService {
     this.headlinesPage++;
     // tslint:disable-next-line:max-line-length
     // return this.http.get<RespuestaTopHeadlines>(`https://newsapi.org/v2/top-headlines?country=us&apiKey=54d8e27c959d419fb2b88d2240ee963c`);
-    return this.ejecutarQuery<RespuestaTopHeadlines>(`/top-headlines?country=us&page=${this.headlinesPage}`);
+    return this.ejecutarQuery<RespuestaTopHeadlines>(`/top-headlines?country=co&page=${this.headlinesPage}`);
   }
 
   getTopHeadlinesCategoria( categoria: string ) {
 
-    if(this.categoriaActual === categoria) {
+    if (this.categoriaActual === categoria) {
       this.categoriaPage++;
     } else {
       this.categoriaPage = 1;
-      this.categoriaActual = categoria; 
+      this.categoriaActual = categoria;
     }
     // tslint:disable-next-line:max-line-length
     // return this.http.get<RespuestaTopHeadlines>(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=54d8e27c959d419fb2b88d2240ee963c`);
-    return this.ejecutarQuery<RespuestaTopHeadlines>(`/top-headlines?country=us&category=${ categoria }&page=${this.categoriaPage}`);
+    return this.ejecutarQuery<RespuestaTopHeadlines>(`/top-headlines?country=co&category=${ categoria }&page=${this.categoriaPage}`);
   }
 }
